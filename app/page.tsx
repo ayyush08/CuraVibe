3177673a214540163e004b378c0b484566655645
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2025-09-16 23:06:26 +0530

    sign-in ui and succesfful github/google auth flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import UserButton from "@/modules/auth/components/user-button";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main className="flex min-h-screen items-center justify-center ">
+      <Button>Click Me</Button>
+      <UserButton/>
+    </main>
   );
 }
--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,10 @@
 //To avoid multiple instances of Prisma Client 
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './generated/prisma/client';
 
 const globalFormPrisma = globalThis as unknown as {
     prisma: PrismaClient
 }
 
 export const db = globalFormPrisma.prisma || new PrismaClient();
-if(process.env.NODE_ENV !== 'production') globalFormPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') globalFormPrisma.prisma = db;
