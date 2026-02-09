de955b199bdd2630603fa38ab3c4e28de278134c
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2026-02-09 19:10:13 +0530

    fix: prisma  import

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 //To avoid multiple instances of Prisma Client 
-// import { PrismaClient } from '@prisma/client';
-import { PrismaClient } from './generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from './generated/prisma/client';
 
 const globalFormPrisma = globalThis as unknown as {
     prisma: PrismaClient
