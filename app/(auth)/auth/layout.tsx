78ace260a67a52fa82766180eb80763445af777c
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2026-02-09 21:42:17 +0530

    feat: add metadata for authentication layout

--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+export const metadata = {
+    title: "Sign In - CuraVibe",
+    description: "Authenticate to access your personalized playgrounds and remote runners on CuraVibe. Log in to manage your coding environments, save your progress, and securely run your code in the cloud. Experience seamless access to your projects from anywhere with CuraVibe's authentication system."
+}
+
 const AuthLayout = ({ children }: {
     children: React.ReactNode
 }) => {
